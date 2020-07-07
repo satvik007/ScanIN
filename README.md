21c17df494b8ef78e4082d1d2ab17e446d7d0e85
Kaushal Kishore <kshr4kshl@gmail.com>
2020-07-07 15:32:07 +0530

    untracking .gitignore files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sample OpenCV NDK integration app:
+
+To setup the same project in your system first download the opencv sdk from this link:
+
+https://sourceforge.net/projects/opencvlibrary/files/4.2.0/opencv-4.2.0-android-sdk.zip/download
+
+
+
+Extract the above zip file.
+
+Then open the app in Android Studion and change the following files:
+
+1. In file `local.properties` change the `sdk.dir` to the Android Sdk location on your system.
+
+2. In file `gradle.properties` change the `opencvsdk` to such that the path `{opencvsdk}/sdk` points to the OpenCV `sdk` folder. For eg. if your OpenCV SDK is located at `/home/kaushal/Android/opencv-4.2.0-android-sdk/OpenCV-android-sdk/sdk` then:
+
+```
+opencvsdk=/home/kaushal/Android/opencv-4.2.0-android-sdk/OpenCV-android-sdk
+```
---
+++ ScanIN/app/src/main/java/com/example/scanin/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.scanin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG="MainActivity";
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java4");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView=(TextView) findViewById(R.id.sample_text);
+        textView.setText(stringFromJNI());
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        if (OpenCVLoader.initDebug()) {
+            textView.setText(textView.getText()+"\n OPENCV LOADED SUCCESSFULLY");
+            textView.setText(textView.getText()+"\n"+validate(500,500));
+
+        } else {
+            Log.d(TAG, "OPENCV DİD NOT LOAD");
+
+        }
+
+
+    }
+
+    /**
+    * A native method that is implemented by the 'native-lib' native library,
+    * which is packaged with this application.
+    */
+    public native String stringFromJNI();
+    public native String validate(long madAddrGr,long matAddrRgba);
+}
+
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Example of a call to a native method
+//        TextView tv = findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+//    }
+//
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    public native String stringFromJNI();
+//}
