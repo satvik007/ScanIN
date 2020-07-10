f03b4789c5882bb4678e42808438f3dd8a2c6f74
unknown <amit_vikram_singh>
2020-07-10 06:30:51 +0530

    Add Grid Adapter

--- ScanIN/app/src/main/java/com/example/scanin/RecylerViewGridAdapter.java
+++ ScanIN/app/src/main/java/com/example/scanin/RecylerViewGridAdapter.java
@@ -1,4 +1,21 @@
 package com.example.scanin;
 
-public class RecylerViewGridAdapter {
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecylerViewGridAdapter extends RecyclerView.Adapter<RecylerViewGridAdapter.GridViewHolder> {
+
+    private String[] mDataset;
+    public static class GridViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public GridViewHolder(TextView v){
+            super(v);
+            textView = v;
+        }
+    }
+
+    public RecylerViewGridAdapter(String[] mDataset){
+
+    }
 }
