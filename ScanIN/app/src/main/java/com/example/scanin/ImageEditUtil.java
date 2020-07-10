60f5f0e18e7c6af302a6b19033aca118dca44496
unknown <amit_vikram_singh>
2020-07-10 15:53:14 +0530

    add filter name list

--- ScanIN/app/src/main/java/com/example/scanin/ImageEditUtil.java
+++ ScanIN/app/src/main/java/com/example/scanin/ImageEditUtil.java
@@ -1,4 +1,11 @@
 package com.example.scanin;
 
+import java.util.Arrays;
+
 public class ImageEditUtil {
+    private static String[] filterList = {"black_and_white", "magic_filter"};
+
+    public static boolean isValidFilter(String filter_name){
+        return Arrays.asList(ImageEditUtil.filterList).contains(filter_name);
+    }
 }
