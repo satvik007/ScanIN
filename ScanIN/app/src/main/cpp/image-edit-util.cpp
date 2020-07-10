828fc0222bc0413f75fbff2de404ad5501d5e399
Kaushal Kishore <kshr4kshl@gmail.com>
2020-07-11 00:28:19 +0530

    calling native library from java

---
+++ ScanIN/app/src/main/cpp/image-edit-util.cpp
@@ -0,0 +1,19 @@
+//
+// Created by kaushal on 10/7/20.
+//
+
+#include <jni.h>
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_scanin_ImageEditUtil_getTestGray(JNIEnv *env, jclass clazz, jlong img_addr,
+                                                  jlong gray_img_addr) {
+    Mat& mGr  = *(Mat*)img_addr;
+    Mat& cvtImg = *(Mat*)gray_img_addr;
+    cvtColor(mGr, cvtImg, COLOR_BGR2GRAY);
+}
