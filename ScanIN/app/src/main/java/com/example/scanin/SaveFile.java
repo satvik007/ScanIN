79b48cb95791bb8c43caed596a403a7f02a08498
Kaushal Kishore <kshr4kshl@gmail.com>
2020-07-11 21:24:49 +0530

    method for saving list of bitmaps to a pdf

--- ScanIN/app/src/main/java/com/example/scanin/SaveFile.java
+++ ScanIN/app/src/main/java/com/example/scanin/SaveFile.java
@@ -1,16 +1,30 @@
 package com.example.scanin;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.pdf.PdfDocument;
 import android.os.Environment;
 import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.scanin.HomeModule.MainActivity;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SaveFile {
+
+    public static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = MainActivity.WRITE_EXTERNAL_STORAGE_REQUEST_CODE;
 
     public static File saveImage(Activity myActivity, Bitmap bitmap) throws IOException {
 
@@ -52,6 +66,75 @@ public class SaveFile {
         } else {
             throw new IOException("This device doesn't have an external storage.");
         }
+    }
+
+
+    public static void savePDF(Activity myActivity, PdfDocument document) throws IOException {
+
+        String externalStorageState = Environment.getExternalStorageState();
+        FileOutputStream myFile = null;
+
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
+
+            File savedPDFDirectory = myActivity.getExternalFilesDir("ScanIN");
+
+            Date currentDate = new Date();
+            long elapsedTime = SystemClock.elapsedRealtime();
+            String uniquePDFName = "/ScanIN_" + currentDate + "_" + elapsedTime + ".pdf";
+
+            myFile = new FileOutputStream(savedPDFDirectory + uniquePDFName);
+
+            document.writeTo(myFile);
+
+            Log.i("PDF", myFile.toString());
+
+        } else {
+            throw new IOException("This device doesn't have an storage.");
+        }
+    }
+
+    public static void createPDFfromListOfBitmap(Activity myActivity,
+                                                 ArrayList<Bitmap> listBmp) throws IOException{
+
+
+        PdfDocument document = new PdfDocument();
+
+        int pageNumber = 1;
+        for (Bitmap bmp : listBmp) {
+
+            // Create a page of the same size as the image
+
+            PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(bmp.getWidth(),
+                    bmp.getHeight(), pageNumber).create();
+
+            PdfDocument.Page page = document.startPage(pageInfo);
+
+            // Draw the bitmap onto the page
+            Canvas canvas = page.getCanvas();
+            canvas.drawBitmap(bmp, 0f, 0f, null);
+            document.finishPage(page);
+
+            pageNumber += 1;
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(myActivity ,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+
+                // write PDF file
+                SaveFile.savePDF(myActivity, document);
+
+                Toast.makeText(myActivity,
+                        "The pdf is saved successfully to external storage.",
+                        Toast.LENGTH_LONG).show();
+
+        } else {
+            ActivityCompat.requestPermissions(myActivity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+        document.close();
     }
 
 }
