022f09fcdd4ae4596cc28b0cd45da5b5c695bd39
Satvik Choudhary <satvikchoudhary@gmail.com>
2020-07-14 10:55:03 +0530

    Image Data and native implementations

--- ScanIN/app/src/main/cpp/image-edit-util.cpp
+++ ScanIN/app/src/main/cpp/image-edit-util.cpp
@@ -2,24 +2,74 @@
 // Created by kaushal on 10/7/20.
 //
 
+#include <iostream>
 #include <jni.h>
 #include <string>
+#include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 
-#include "include/filters.hpp"
-
+#include "filters.hpp"
+#include "corners.hpp"
 using namespace cv;
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_scanin_ImageDataModule_ImageEditUtil_getTestGray(JNIEnv *env, jclass clazz, jlong img_addr,
-                                                  jlong gray_img_addr) {
-    Mat& mGr  = *(Mat*)img_addr;
-    Mat& cvtImg = *(Mat*)gray_img_addr;
+Java_com_example_scanin_ImageDataModule_ImageEditUtil_cropImage(JNIEnv *env, jclass clazz,
+                                                                jlong img_addr, jlong crop_img_addr,
+                                                                jlong pts) {
+    Mat& src = *(Mat*) img_addr;
+    Mat& dst = *(Mat*) crop_img_addr;
+    Mat& points = *(Mat*) pts;
+    std::vector <cv::Point> vec_pts (points);
 
-    //magic_filter(mGr, cvtImg);
-    dark_magic_filter(mGr, cvtImg);
+    // changing cv::INTER_NEAREST to cv::INTER_LANCZOS4 should improve results
+    // but degrade performance.
+    // This can raise errors due to order problem.
+    int ret = four_point_transform(src, dst, vec_pts, cv::INTER_NEAREST);
+    if (ret) {
+        std::cerr << "transform failed due to order issue." << std::endl;
+    }
+}
 
-//    cvtColor(mGr, cvtImg, COLOR_BGR2GRAY);
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_scanin_ImageDataModule_ImageEditUtil_getTestGray(JNIEnv *env, jclass clazz,
+                                                                  jlong img_addr,
+                                                                  jlong gray_img_addr) {
+    Mat &src = *(Mat*) img_addr;
+    Mat &dst = *(Mat*) gray_img_addr;
+
+    dark_magic_filter(src, dst);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_scanin_ImageDataModule_ImageEditUtil_filterImage(JNIEnv *env, jclass clazz,
+                                                                  jlong img_addr,
+                                                                  jlong filter_img_addr,
+                                                                  jint filter_id) {
+    Mat &src = *(Mat*) img_addr;
+    Mat &dst = *(Mat*) filter_img_addr;
+
+    // filterList = {"magic_filter", "gray_filter", "dark_magic_filter", "sharpen_filter"};
+    switch (filter_id) {
+        case 0: magic_filter(src, dst); break;
+        case 1: gray_filter(src, dst); break;
+        case 2: dark_magic_filter(src, dst); break;
+        case 3: sharpen_filter(src, dst); break;
+        default: std::cerr << "We should never reach here." << std::endl;
+    }
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_scanin_ImageDataModule_ImageEditUtil_getBestPoints(JNIEnv *env, jclass clazz,
+                                                                    jlong img_addr, jlong pts) {
+    Mat &src = *(Mat*) img_addr;
+    Mat &res = *(Mat*) pts;
+    std::vector <Point> vec_pts;
+
+    find_best_corners(src, vec_pts);
+    res = Mat(vec_pts);
 }
