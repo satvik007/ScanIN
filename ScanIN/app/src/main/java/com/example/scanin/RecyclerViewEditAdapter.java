29f4a1b5348173cb01df09730f2f30153dfef212
Satvik Choudhary <satvikchoudhary@gmail.com>
2020-07-16 01:33:49 +0530

    Polygon View modified + RecycleViewEditAdapter small changes

--- ScanIN/app/src/main/java/com/example/scanin/RecyclerViewEditAdapter.java
+++ ScanIN/app/src/main/java/com/example/scanin/RecyclerViewEditAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,12 +17,13 @@ import java.util.ArrayList;
 
 public class RecyclerViewEditAdapter extends RecyclerView.Adapter<RecyclerViewEditAdapter.EditViewHolder> {
     private ArrayList<ImageData> mDataset = new ArrayList<ImageData>();
+    private ProgressBar progressBar;
 
     public class EditViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
         public EditViewHolder(View view){
             super(view);
-            imageView =view.findViewById(R.id.image_edit_item);
+            imageView =view.findViewById(R.id.holderImageCrop);
         }
     }
 
@@ -29,10 +32,31 @@ public class RecyclerViewEditAdapter extends RecyclerView.Adapter<RecyclerViewEd
     }
 
     public RecyclerViewEditAdapter.EditViewHolder onCreateViewHolder(ViewGroup parent, int viewtype){
-        int layoutIdForImageAdapter =R.layout.image_edit_item;
+        int layoutIdForImageAdapter =R.layout.image_edit_crop;
         LayoutInflater inflater =LayoutInflater.from(parent.getContext());
         View view =inflater.inflate(layoutIdForImageAdapter, parent, false);
         return new EditViewHolder(view);
+    }
+
+    private void setViewInteract(View view, boolean canDo) {
+        view.setEnabled(canDo);
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                setViewInteract(((ViewGroup) view).getChildAt(i), canDo);
+            }
+        }
+    }
+
+    protected void showProgressBar(View view) {
+        RelativeLayout rlContainer = view.findViewById(R.id.rlContainer);
+        setViewInteract(rlContainer, false);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    protected void hideProgressBar(View view) {
+        RelativeLayout rlContainer = view.findViewById(R.id.rlContainer);
+        setViewInteract(rlContainer, true);
+        progressBar.setVisibility(View.GONE);
     }
 
 //    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
