90ddd8331dcd1d2a017c26fd62fdb4e10d18ece6
amitkvikram <amitkvikram@gmail.com>
2020-07-20 23:54:59 +0530

    update edit tools ui

--- ScanIN/app/src/main/java/com/example/scanin/HomeModule/RecyclerViewDocAdapter.java
+++ ScanIN/app/src/main/java/com/example/scanin/HomeModule/RecyclerViewDocAdapter.java
@@ -76,10 +76,10 @@ public class RecyclerViewDocAdapter extends RecyclerView.Adapter<RecyclerViewDoc
         }
 
         if(position%2==0){
-            holder.itemView.setPadding(0, 10, 40, 0);
+            holder.itemView.setPadding(0, 30, 40, 0);
         }
         else{
-            holder.itemView.setPadding(40, 10, 0, 0);
+            holder.itemView.setPadding(40, 30, 0, 0);
         }
 
         int size = (int) Math.ceil(Math.sqrt(MAX_WIDTH * MAX_HEIGHT));
